ceb0b1b5e51e61e54eeb8b1f30297b5524058ded
f0t0n <t.34.oxygen@gmail.com>
2012-08-13 20:12:56 +0300

    move js to external file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 (function() {
     $(function($, window) {
         $('a.btn').button();
+        $("#tabs").tabs({
+            cookie: {
+                // store cookie for a day, without, it would be a session cookie
+                expires: 1,
+                path: '/tabs'
+            }
+        });
     });
 })(jQuery, this);
